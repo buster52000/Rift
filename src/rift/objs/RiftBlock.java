cc2a9cab32f7abc3618eebc9319b85470f4f724a
Buster52000 <buster52000@gmail.com>
2013-05-28 11:05:23 -0400

    Added RiftBlock class, disallowing rift placing in certain areas

---
+++ src/rift/objs/RiftBlock.java
@@ -0,0 +1,54 @@
+package rift.objs;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+import rift.RObj;
+import rift.UI;
+
+public class RiftBlock extends RObj {
+
+	private JPanel panel;
+	
+	public RiftBlock(int locationX, int locationY, int height, int width) {
+		super(7, locationX, locationY, height, width, false, -1);
+		panel = new JPanel();
+		panel.setBackground(Color.LIGHT_GRAY);
+	}
+
+	
+	public void resize() {
+		aX = UI.getScaledWidth(getX());
+		aY = UI.getScaledHeight(getY());
+		aHeight = UI.getScaledHeight(getHeight());
+		aWidth = UI.getScaledWidth(getWidth());
+		panel.setSize(aWidth, aHeight);
+		panel.setPreferredSize(new Dimension(aWidth, aHeight));
+		panel.setLocation(aX, aY);
+		panel.setMaximumSize(panel.getPreferredSize());
+		panel.setMinimumSize(panel.getPreferredSize());		
+	}
+	
+	public JPanel getPanel() {
+		return panel;
+	}
+
+	public boolean canIntersect() {		
+		return true;
+	}
+
+	public boolean canFireThrough() {		
+		return true;
+	}
+
+	public int constantAction(ArrayList<RObj> objs) {		
+		return 0;
+	}
+
+	public int action(ArrayList<RObj> objs, int actionBy) {
+		return 0;
+	}
+}
